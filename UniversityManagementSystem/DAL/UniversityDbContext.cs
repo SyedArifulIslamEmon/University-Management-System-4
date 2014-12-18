cba0366d067df1aabefcfb39b9fa319579593338
S. M. Saifur Rahman <saifur.rahman25@gmail.com>
2014-12-19 03:15:56 +0600

    sem, dep, course, dbcontex added

---
+++ UniversityManagementSystem/DAL/UniversityDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+//using UniversityManagementSystem.Migrations;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using UniversityManagementSystem.Models;
+
+namespace UniversityManagementSystem.DAL
+{
+    public class UniversityDbContext : DbContext
+    {
+        public UniversityDbContext() : base("UniversityConnection") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<UniversityDbContext,Configuration>());
+            //base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Department> Departments { get; set; }
+
+        public DbSet<Semester> Semesters { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ UniversityManagementSystem/Models/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityManagementSystem.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+
+        public string Code { get; set; }
+
+        public int Credit { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int DepartmentId { get; set; }
+        public virtual Department Department { get; set; }
+
+        public int SemesterId { get; set; }
+        public virtual Semester Semester { get; set; }
+
+        
+    }
+}
