443bc23f488d99cf926cbd54c41b47864968b242
S. M. Saifur Rahman <saifur.rahman25@gmail.com>
2014-12-20 00:17:19 +0600

    Teacher added

---
+++ UniversityManagementSystem/DAL/UniversityInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using UniversityManagementSystem.Models;
+
+namespace UniversityManagementSystem.DAL
+{
+    public class UniversityInitializer : DropCreateDatabaseIfModelChanges<UniversityDbContext>
+    {
+        protected override void Seed(UniversityDbContext context)
+        {
+            var designation = new List<Designation>
+            {
+                new Designation {Name = "Professor"},
+                new Designation {Name = "Asst. Professor"},
+                new Designation {Name = "Lecturer"},
+                new Designation {Name = "Asst. Lecturer"}
+            };
+
+            designation.ForEach(d => context.Designations.Add(d));
+            context.SaveChanges();
+        }
+    }
+}
--- UniversityManagementSystem/Global.asax.cs
+++ UniversityManagementSystem/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using UniversityManagementSystem.DAL;
 
 namespace UniversityManagementSystem
 {
@@ -13,6 +15,7 @@ namespace UniversityManagementSystem
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            Database.SetInitializer(new UniversityInitializer());
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
